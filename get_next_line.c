ccd204d0dbe177f772f987e87c85a0dd8944e559
Four Graceford <fgracefo@ox-h2.21-school.ru>
2019-10-05 16:56:16 +0300

    3

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,39 @@
 /*   By: fgracefo <fgracefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 17:47:04 by fgracefo          #+#    #+#             */
-/*   Updated: 2019/10/05 14:45:20 by fgracefo         ###   ########.fr       */
+/*   Updated: 2019/10/05 16:54:45 by fgracefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	to_check(char *last, char **line)
+{
+	char		*pointer_find;
+
+	pointer_find = NULL;
+	if (last)
+	{
+		if ((pointer_find = ft_strchr(last, '\n')))
+		{
+			*pointer_find++ = '\0';
+			*line = ft_strdup(last);
+			ft_strcpy(last, pointer_find);
+			free(last);
+		}
+		else
+		{
+			*line = ft_strdup(last);
+			ft_strclr(last);
+			free(last);
+		}
+	}
+	else
+	{
+		*line = ft_strnew(1);
+	}
+	return (pointer_find);
+}
 
 char	*ft_strjoinf(char *s1, char *s2)
 {
@@ -26,57 +54,67 @@ int		get_next_line(const int fd, char **line)
 	static char	*last;
 	char		buf[BUFF_SIZE + 1];
 	int			how_much;
-	char		*pointer_find;
+	char		*pointer_del;
+	char		*pointer_f;
 
-	pointer_find = NULL;
 	if (fd < 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
+	pointer_f = to_check(last, line);
 	if (last)
 	{
-		if ((pointer_find = ft_strchr(last, '\n')))
+		if ((pointer_f = ft_strchr(last, '\n')))
 		{
-			*pointer_find++ = '\0';
+			*pointer_f++ = '\0';
 			*line = ft_strdup(last);
-			ft_strcpy(last, pointer_find);
+			ft_strcpy(last, pointer_f);
+			free(last);
 		}
 		else
 		{
 			*line = ft_strdup(last);
 			ft_strclr(last);
+			free(last);
 		}
 	}
 	else
 		*line = ft_strnew(1);
 		
-	while((!pointer_find) &&(how_much = read(fd, buf, BUFF_SIZE))) // pointer_find в строчку 41 становится не нулевым, в случае если он стал
+	while((!pointer_f) &&(how_much = read(fd, buf, BUFF_SIZE)))
  	{
 		buf[how_much] = '\0';
-		if ((pointer_find = ft_strchr(buf, '\n')))
+		if ((pointer_f = ft_strchr(buf, '\n')))
 		{
-			*pointer_find = '\0';
-			pointer_find++;
-			last = ft_strdup(pointer_find);
+			*pointer_f = '\0';
+			pointer_f++;
+			last = ft_strdup(pointer_f);
 		}
+		// pointer_del = *line;
 		*line = ft_strjoinf(*line, buf);
+		// free(pointer_del);
 	}
 	return ((how_much || ft_strlen(last) || ft_strlen(*line)) ? 1 : 0);
 }
-// int	main(int argc, char **argv)
-// {
-// 	char	*line;
-// 	int		fd;
-// 	int i = 1;
+
+int	main(int argc, char **argv)
+{
+	char	*line;
+	int		fd;
+	int i = 1;
 	
-// 	if (argc == 2)
-// 	{
-// 		fd = open(argv[1], O_RDONLY);
-// 		while (get_next_line(fd, &line))
-// 		{
-// 			printf("заходим в цикл %d\n", i);
-// 			printf("%s\n", line);
-// 			i++;
-// 			free(line);
-// 		}
-// 	}
-// 	return (0);
-// }
+	// if (argc == 2)
+	// {
+		fd = open(argv[1], O_RDONLY);
+	// 	while (get_next_line(fd, &line))
+	// 	{
+	// 		printf("заходим в цикл %d\n", i);
+	// 		printf("%s\n", line);
+	// 		i++;
+	// 		free(line);
+	// 	}
+	// }
+	get_next_line(fd, &line);
+		// {
+			printf("заходим в цикл %d\n", i);
+			printf("%s\n", line);
+	return(0);
+}
