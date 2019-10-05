eaef2a5662519bd295f0d35620ae03082b610e27
Four Graceford <fgracefo@ox-h2.21-school.ru>
2019-10-05 18:24:05 +0300

    last

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: fgracefo <fgracefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 17:47:04 by fgracefo          #+#    #+#             */
-/*   Updated: 2019/10/05 17:37:45 by fgracefo         ###   ########.fr       */
+/*   Updated: 2019/10/05 18:17:41 by fgracefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	to_check(char *last, char **line)
+char	*to_check(char *last, char **line)
 {
 	char		*pointer_find;
 
@@ -24,13 +24,11 @@ char	to_check(char *last, char **line)
 			*pointer_find++ = '\0';
 			*line = ft_strdup(last);
 			ft_strcpy(last, pointer_find);
-			free(last);
 		}
 		else
 		{
 			*line = ft_strdup(last);
 			ft_strclr(last);
-			free(last);
 		}
 	}
 	else
@@ -54,14 +52,13 @@ int		get_next_line(const int fd, char **line)
 	static char	*last;
 	char		buf[BUFF_SIZE + 1];
 	int			how_much;
-	char		*pointer_del;
 	char		*pointer_f;
 
 	if (fd < 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
 	pointer_f = to_check(last, line);
-	while((!pointer_f) &&(how_much = read(fd, buf, BUFF_SIZE)))
- 	{
+	while ((!pointer_f) && (how_much = read(fd, buf, BUFF_SIZE)))
+	{
 		buf[how_much] = '\0';
 		if ((pointer_f = ft_strchr(buf, '\n')))
 		{
@@ -71,5 +68,7 @@ int		get_next_line(const int fd, char **line)
 		}
 		*line = ft_strjoinf(*line, buf);
 	}
-	return ((how_much || ft_strlen(last) || ft_strlen(*line)) ? 1 : 0);
+	if (how_much == 0 && !(ft_strlen(*line)))
+		return (0);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fgracefo <fgracefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 17:52:26 by fgracefo          #+#    #+#             */
-/*   Updated: 2019/10/05 14:35:19 by fgracefo         ###   ########.fr       */
+/*   Updated: 2019/10/05 17:47:32 by fgracefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # define BUFF_SIZE 10
 
 # include <stdio.h>
-# include <fcntl.h> // open
-# include <unistd.h> //read write
+# include <fcntl.h>
+# include <unistd.h>
 # include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
