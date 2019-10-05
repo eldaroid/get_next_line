fc15e78ec65ab4b11b72a464d289eda1045f2223
Four Graceford <fgracefo@ox-h2.21-school.ru>
2019-10-05 17:38:11 +0300

    pass this project

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fgracefo <fgracefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 17:47:04 by fgracefo          #+#    #+#             */
-/*   Updated: 2019/10/05 16:54:45 by fgracefo         ###   ########.fr       */
+/*   Updated: 2019/10/05 17:37:45 by fgracefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,25 +60,6 @@ int		get_next_line(const int fd, char **line)
 	if (fd < 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
 	pointer_f = to_check(last, line);
-	if (last)
-	{
-		if ((pointer_f = ft_strchr(last, '\n')))
-		{
-			*pointer_f++ = '\0';
-			*line = ft_strdup(last);
-			ft_strcpy(last, pointer_f);
-			free(last);
-		}
-		else
-		{
-			*line = ft_strdup(last);
-			ft_strclr(last);
-			free(last);
-		}
-	}
-	else
-		*line = ft_strnew(1);
-		
 	while((!pointer_f) &&(how_much = read(fd, buf, BUFF_SIZE)))
  	{
 		buf[how_much] = '\0';
@@ -88,33 +69,7 @@ int		get_next_line(const int fd, char **line)
 			pointer_f++;
 			last = ft_strdup(pointer_f);
 		}
-		// pointer_del = *line;
 		*line = ft_strjoinf(*line, buf);
-		// free(pointer_del);
 	}
 	return ((how_much || ft_strlen(last) || ft_strlen(*line)) ? 1 : 0);
-}
-
-int	main(int argc, char **argv)
-{
-	char	*line;
-	int		fd;
-	int i = 1;
-	
-	// if (argc == 2)
-	// {
-		fd = open(argv[1], O_RDONLY);
-	// 	while (get_next_line(fd, &line))
-	// 	{
-	// 		printf("заходим в цикл %d\n", i);
-	// 		printf("%s\n", line);
-	// 		i++;
-	// 		free(line);
-	// 	}
-	// }
-	get_next_line(fd, &line);
-		// {
-			printf("заходим в цикл %d\n", i);
-			printf("%s\n", line);
-	return(0);
 }
